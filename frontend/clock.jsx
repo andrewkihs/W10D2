1bd3202e1ea7220c854bf40e866820767c547586
aturi <angelaturi99@gmail.com>
2021-04-27 14:58:40 -0400

    Finish #componentDidMount, work on #componentWillUnmount, #render, start styling

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -9,10 +9,16 @@ class Clock extends React.Component {
     }
 
     this.tick = this.tick.bind(this);
+    // this.now = this.now.bind(this)
   }
 
   componentDidMount() {
-    console.log('component mounted')
+    this.intervalID = setInterval(this.tick, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+    this.intervalID = null;
   }
   
   tick(){
@@ -22,10 +28,17 @@ class Clock extends React.Component {
   }  
 
   render () {
-    console.log('rendered')
+    let hours = this.state.time.getHours();
+    let minutes = this.state.time.getMinutes();
+    let seconds = this.state.time.getSeconds();
     return (
       <div className="clock">
         <h1>Clock</h1>
+        <span>Time: </span>
+        <span>{hours}:{minutes}:{seconds}</span>
+        <br/>
+        <span>Date: </span>
+        <span>{this.state.time.toDateString()}</span>
       </div>
     )
   }
